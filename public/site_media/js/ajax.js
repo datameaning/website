12ac8e1cd3cdd3fa37b025fc72aac5a84eeb7e6a
Data Meaning <datameaning2@gmail.com>
2011-09-21 16:13:44 +0200

    main css transfered to external file and firs js script

---
+++ public/site_media/js/ajax.js
@@ -0,0 +1,19 @@
+
+
+/* alert("Coucou"); */
+
+/*
+$.ajax({
+       url : 'http://localhost:8002/website/',
+       dataType : 'html',
+       type : 'GET',
+       success : function(response) {
+       document.write(response); 
+       alert("Poursuivre");
+        }
+       }) ;
+
+*/
+
+
+
